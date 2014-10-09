852e3da6fd68534e4d48328a7fa6f62fa9dc77d3
{xeena} <{xeena@gmail.com}>
2014-10-09 15:38:47 +0900

    validations plus

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,10 @@ class Page < ActiveRecord::Base
 	has_and_belongs_to_many :editors, :class_name => "AdminUser"
 
 	validates_presence_of	:name
+	validates_length_of		:name, :maximum => 255
+	validates_presence_of	:permalink
+	validates_length_of		:permalink, :within => 3..255
+	validates_uniqueness_of :permalink
 
 	scope :visible, lambda { where(:visible => true) }
 	scope :invisible, lambda { where(:visible => false) }
